80b0a60f9adb93b22d21426610d87542ded7d0a3
Vincent Salinas <vince@Vincents-MacBook-Pro.local>
2022-05-26 20:03:49 -0700

    Finished designing layout for signup page

---
+++ FunZoneApp/Controllers/Login/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  FunZoneApp
+//
+//  Created by Vincent Salinas on 5/26/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FunZoneApp/Controllers/SignUp/SignUpViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SignUpViewController.swift
+//  FunZoneApp
+//
+//  Created by Vincent Salinas on 5/26/22.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    @IBOutlet weak var box1: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        box1.layer.cornerRadius = 10
+        box1.layer.masksToBounds = true
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
