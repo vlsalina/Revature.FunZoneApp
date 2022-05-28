90919e8cdb8f442f6ffbbe32fe1d59d8ac1ebf6e
Vincent Salinas <vince@Vincents-MacBook-Pro.local>
2022-05-28 15:58:45 -0700

    Designing movie page

--- FunZoneApp/Classes/Movies.swift
+++ FunZoneApp/Classes/Movies.swift
@@ -7,29 +7,31 @@
 
 import UIKit
 
-class Movie {
+class Movies {
     
     var title = ""
     var year : Int
     var imageMovie : UIImage
     var rating = ""
+    var type = ""
     
     
-    init(title:String , year:Int, imageMovie:UIImage, rating:String  ) {
+    init(title:String , year:Int, imageMovie:UIImage, rating:String, type:String) {
         self.title = title
         self.year = year
         self.imageMovie = imageMovie
         self.rating = rating
+        self.type = ""
     }
     
-    static func FetchMovie () -> [Movie]{
+    static func FetchMovie () -> [Movies]{
         
         return [
-            Movie(title: "The Art of Sketching ", year: 2000, imageMovie: UIImage(named: "1" )!, rating: "PG"),
-            Movie(title: "The Art of Sketching ", year: 2000, imageMovie: UIImage(named: "1" )!, rating: "PG"),
-            Movie(title: "The Art of Sketching ", year: 2000, imageMovie: UIImage(named: "1" )!, rating: "PG"),
-            Movie(title: "The Art of Sketching ", year: 2000, imageMovie: UIImage(named: "1" )!, rating: "PG"),
-        ]
+            Movies(title: "The Art of Sketching ", year: 2000, imageMovie: UIImage(named: "1" )!, rating: "PG", type: "Movie"),
+            Movies(title: "The Art of Sketching ", year: 2000, imageMovie: UIImage(named: "1" )!, rating: "PG", type: "Movie"),
+            Movies(title: "The Art of Sketching ", year: 2000, imageMovie: UIImage(named: "1" )!, rating: "PG", type: "Movie"),
+            Movies(title: "The Art of Sketching ", year: 2000, imageMovie: UIImage(named: "1" )!, rating: "PG", type: "Movie"),
+       ]
         
     }
 }
---
+++ FunZoneApp/Controllers/MoviesPage/MovieCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MovieCollectionViewCell.swift
+//  FunZoneApp
+//
+//  Created by Vincent Salinas on 5/28/22.
+//
+
+import UIKit
+
+class MovieCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var movieImage: UIImageView!
+    @IBOutlet weak var year: UILabel!
+    @IBOutlet weak var type: UILabel!
+    @IBOutlet weak var rating: UILabel!
+    @IBOutlet weak var title: UILabel!
+}
