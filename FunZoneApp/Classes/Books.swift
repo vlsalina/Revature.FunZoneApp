5a44dde6bf8dd340271aeac6bf8ed77378a924ff
Vincent Salinas <vince@Vincents-MacBook-Pro.local>
2022-05-29 12:19:38 -0700

    Implemented book collection view

---
+++ FunZoneApp/Classes/Books.swift
@@ -0,0 +1,8 @@
+//
+//  Books.swift
+//  FunZoneApp
+//
+//  Created by Vincent Salinas on 5/29/22.
+//
+
+import Foundation
---
+++ FunZoneApp/Controllers/BooksPage/BookCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  BookCollectionViewCell.swift
+//  FunZoneApp
+//
+//  Created by Vincent Salinas on 5/29/22.
+//
+
+import UIKit
+
+class BookCollectionViewCell: UICollectionViewCell {
+    
+}
