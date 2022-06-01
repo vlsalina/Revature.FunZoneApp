001a01d831bbdb64882c635b52a062c20c28ce04
Vincent Salinas <vince@Vincents-MacBook-Pro.local>
2022-05-31 18:05:26 -0700

    Finished implementing notes collection view cell controller

--- FunZoneApp/Controllers/NotesPage/NotesCollectionViewCell.swift
+++ FunZoneApp/Controllers/NotesPage/NotesCollectionViewCell.swift
@@ -9,4 +9,21 @@ import UIKit
 
 class NotesCollectionViewCell: UICollectionViewCell {
     
+    @IBOutlet weak var NoteTitle: UILabel!
+    @IBOutlet weak var NoteDescription: UILabel!
+    @IBOutlet weak var NoteImageView: UIImageView!
+    
+    var note : Notes! {
+        didSet {
+            self.updateUI()
+        }
+    }
+    
+    func updateUI() {
+        if let note = note {
+            NoteTitle.text = note.title
+            NoteDescription.text = note.description
+            NoteImageView.image = note.imageNotes
+        }
+    }
 }
