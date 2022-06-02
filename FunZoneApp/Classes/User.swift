ff9f05a2d9907bd5fc591c03dd04d335d34c75fa
Vincent Salinas <vince@Vincents-MacBook-Pro.local>
2022-06-02 15:03:48 -0700

    Created user class

---
+++ FunZoneApp/Classes/User.swift
@@ -0,0 +1,20 @@
+//
+//  User.swift
+//  FunZoneApp
+//
+//  Created by Vincent Salinas on 6/2/22.
+//
+
+import Foundation
+
+class User {
+    
+    var email : String
+    var password : String
+    
+    init(email: String, password: String) {
+        self.email = email
+        self.password = password
+    }
+    
+}
