dab8fbe3bd7cd788f506fc2e177c8cf3e58aedb5
Vincent Salinas <vince@Vincents-MacBook-Pro.local>
2022-06-05 00:33:57 -0700

    Recreated note page

--- FunZoneApp/Controllers/NotePage/NotePageViewController.swift
+++ FunZoneApp/Controllers/NotePage/NotePageViewController.swift
@@ -19,20 +19,9 @@ class NotePageViewController: UIViewController {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
-        noteTableView.dataSource = self
-        initialize()
         
     }
     
-    func initialize() {
-        noteContents = [note?.title, note?.desc, note?.body]
-    }
-    
-    @IBAction func editNote(_ sender: Any) {
-        let editNoteVC = getTarget(target: "EditNotePageViewController") as! EditNotePageViewController
-        editNoteVC.note = note
-        segueToVC(target: "EditNotePageViewController", sender: self)
-    }
     
     //    @IBAction func deleteNote(_ sender: Any) {
 //        let status = DBHelperClass.dbHelper.deleteNote(title: note!.title!)
@@ -59,21 +48,4 @@ class NotePageViewController: UIViewController {
     
 }
 
-extension NotePageViewController : UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return noteContents.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = noteTableView.dequeueReusableCell(withIdentifier: "noteCell", for: indexPath)
-        
-        cell.textLabel?.text = noteContents[indexPath.row]
-        cell.textLabel?.numberOfLines  = 0
-        cell.textLabel?.textColor = UIColor.white
-        
-        return cell
-        
-    }
-    
-    
-}
+
